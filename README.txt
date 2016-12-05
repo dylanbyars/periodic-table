5919b7afdf3bdf9472c6cebb48ae950757506700
Dylan <dylanbyars@dbyars.home>
2016-12-05 07:27:41 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/dylanbyars/pen/OMxYRq.
+
+A responsive periodic table built with a whole mess of flex boxes. 
---
+++ js/index.js
@@ -0,0 +1,700 @@
+const elementInfo = {
+  H: {
+    name: "Hydrogen",
+    radioactive: false,
+    category: "Non-metal"
+      // date discovered
+      // discoverer
+      // unexpected symbol
+      // interesting facts
+  },
+  He: {
+    name: "Helium",
+    radioactive: false,
+    category: "Noble Gas"
+  },
+  Li: {
+    name: "Lithium",
+    radioactive: false,
+    category: "Alkali Metal"
+  },
+  Be: {
+    name: "Berylium",
+    radioactive: false,
+    category: "Alkaline Earth Metal"
+  },
+  B: {
+    name: "Boron",
+    radioactive: false,
+    category: "Metalloid"
+  },
+  C: {
+    name: "Carbon",
+    radioactive: false,
+    category: "Non-metal"
+  },
+  N: {
+    name: "Nitrogen",
+    radioactive: false,
+    category: "Non-metal"
+  },
+  O: {
+    name: "Oxygen",
+    radioactive: false,
+    category: "Non-metal"
+  },
+  F: {
+    name: "Flourine",
+    radioactive: false,
+    category: "Halogen"
+  },
+  Ne: {
+    name: "Neon",
+    radioactive: false,
+    category: "Noble Gas"
+  },
+  Na: {
+    name: "Sodium",
+    radioactive: false,
+    category: "Alkali Metal"
+  },
+  Mg: {
+    name: "Magnesium",
+    radioactive: false,
+    category: "Alkaline Earth Metal"
+  },
+  Al: {
+    name: "Aluminium",
+    radioactive: false,
+    category: "Post-transition Metal"
+  },
+  Si: {
+    name: "Silicon",
+    radioactive: false,
+    category: "Metalloid"
+  },
+  P: {
+    name: "Phosphorous",
+    radioactive: false,
+    category: "Non-metal"
+  },
+  S: {
+    name: "Sulfur",
+    radioactive: false,
+    category: "Non-metal"
+  },
+  Cl: {
+    name: "Chlorine",
+    radioactive: false,
+    category: "Halogen"
+  },
+  Ar: {
+    name: "Argon",
+    radioactive: false,
+    category: "Noble Gas"
+  },
+  K: {
+    name: "Potassium",
+    radioactive: false,
+    category: "Alkali Metal"
+  },
+  Ca: {
+    name: "Calcium",
+    radioactive: false,
+    category: "Alkaline Earth Metal"
+  },
+  Sc: {
+    name: "Scandium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Ti: {
+    name: "Titanium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  V: {
+    name: "Vanadium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Cr: {
+    name: "Chromium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Mn: {
+    name: "Manganese",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Fe: {
+    name: "Iron",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Co: {
+    name: "Cobalt",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Ni: {
+    name: "Nickel",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Cu: {
+    name: "Copper",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Zn: {
+    name: "Zinc",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Ga: {
+    name: "Gallium",
+    radioactive: false,
+    category: "Post-transition Metal"
+  },
+  Ge: {
+    name: "Germanium",
+    radioactive: false,
+    category: "Metalloid"
+  },
+  As: {
+    name: "Arsenic",
+    radioactive: false,
+    category: "Metalloid"
+  },
+  Se: {
+    name: "Selenium",
+    radioactive: false,
+    category: "Non-metal"
+  },
+  Br: {
+    name: "Bromine",
+    radioactive: false,
+    category: "Halogen"
+  },
+  Kr: {
+    name: "Krypton",
+    radioactive: false,
+    category: "Noble Gas"
+  },
+  Rb: {
+    name: "Rubidium",
+    radioactive: false,
+    category: "Alkali Metal"
+  },
+  Sr: {
+    name: "Strontium",
+    radioactive: false,
+    category: "Alkaline Earth Metal"
+  },
+  Y: {
+    name: "Yttrium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Zr: {
+    name: "Zirconium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Nb: {
+    name: "Niobium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Mo: {
+    name: "Molybdenum",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Tc: {
+    name: "Technetium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Ru: {
+    name: "Ruthenium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Rh: {
+    name: "Rhodium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Pd: {
+    name: "Palladium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Ag: {
+    name: "Silver",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Cd: {
+    name: "Cadmium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  In: {
+    name: "Indium",
+    radioactive: false,
+    category: "Post-transition Metal"
+  },
+  Sn: {
+    name: "Tin",
+    radioactive: false,
+    category: "Post-transition Metal"
+  },
+  Sb: {
+    name: "Antimony",
+    radioactive: false,
+    category: "Metalloid"
+  },
+  Te: {
+    name: "Tellurium",
+    radioactive: false,
+    category: "Metalloid"
+  },
+  I: {
+    name: "Iodine",
+    radioactive: false,
+    category: "Halogen"
+  },
+  Xe: {
+    name: "Xenon",
+    radioactive: false,
+    category: "Noble Gas"
+  },
+  Cs: {
+    name: "Cesium",
+    radioactive: false,
+    category: "Alkali Metal"
+  },
+  Ba: {
+    name: "Barium",
+    radioactive: false,
+    category: "Alkaline Earth Metal"
+  },
+  La: {
+    name: "Lanthanum",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Ce: {
+    name: "Cerium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Pr: {
+    name: "Praseodymium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Nd: {
+    name: "Neodymium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Pm: {
+    name: "Promethium",
+    radioactive: true,
+    category: "Lanthanoid"
+  },
+  Sm: {
+    name: "Samarium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Eu: {
+    name: "Europium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Gd: {
+    name: "Gadolinium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Tb: {
+    name: "Terbium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Dy: {
+    name: "Dysprosium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Ho: {
+    name: "Holmium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Er: {
+    name: "Erbium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Tm: {
+    name: "Thulium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Yb: {
+    name: "Ytterbium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Lu: {
+    name: "Lutetium",
+    radioactive: false,
+    category: "Lanthanoid"
+  },
+  Hf: {
+    name: "Hafnium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Ta: {
+    name: "Tantalum",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  W: {
+    name: "Tungsten",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Re: {
+    name: "Rhenium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Os: {
+    name: "Osmium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Ir: {
+    name: "Iridium",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Pt: {
+    name: "Platinum",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Au: {
+    name: "Gold",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Hg: {
+    name: "Mercury",
+    radioactive: false,
+    category: "Transition Metal"
+  },
+  Tl: {
+    name: "Thallium",
+    radioactive: false,
+    category: "Post-transition Metal"
+  },
+  Pb: {
+    name: "Lead",
+    radioactive: false,
+    category: "Post-transition Metal"
+  },
+  Bi: {
+    name: "Bismuth",
+    radioactive: false,
+    category: "Post-transition Metal"
+  },
+  Po: {
+    name: "Polonium",
+    radioactive: true,
+    category: "Metalloid"
+  },
+  At: {
+    name: "Astatine",
+    radioactive: true,
+    category: "Halogen"
+  },
+  Rn: {
+    name: "Radon",
+    radioactive: true,
+    category: "Noble Gas"
+  },
+  Fr: {
+    name: "Francium",
+    radioactive: true,
+    category: "Alkali Metal"
+  },
+  Ra: {
+    name: "Radium",
+    radioactive: true,
+    category: "Alkaline Earth Metal"
+  },
+  Ac: {
+    name: "Actinium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Th: {
+    name: "Thorium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Pa: {
+    name: "Protactinium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  U: {
+    name: "Uranium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Np: {
+    name: "Neptunium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Pu: {
+    name: "Plutonium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Am: {
+    name: "Americium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Cm: {
+    name: "Curium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Bk: {
+    name: "Berylium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Cf: {
+    name: "Californium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Es: {
+    name: "Einsteinium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Fm: {
+    name: "Fermium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Md: {
+    name: "Mendelevium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  No: {
+    name: "Nobelium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Lr: {
+    name: "Lawrencium",
+    radioactive: true,
+    category: "Actinoid"
+  },
+  Rf: {
+    name: "Rutherfordium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Db: {
+    name: "Dubnium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Sg: {
+    name: "Seaborgium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Bh: {
+    name: "Bohrium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Hs: {
+    name: "Hassium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Mt: {
+    name: "Meitnerium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Ds: {
+    name: "Darmstadtium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Rg: {
+    name: "Roentgenium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Cn: {
+    name: "Copernicium",
+    radioactive: true,
+    category: "Transition Metal"
+  },
+  Nh: {
+    name: "Nihonium",
+    radioactive: true,
+    category: "Post-transition Metal"
+  },
+  Fl: {
+    name: "Flerovium",
+    radioactive: true,
+    category: "Post-transition Metal"
+  },
+  Mc: {
+    name: "Moscovium",
+    radioactive: true,
+    category: "Post-transition Metal"
+  },
+  Lv: {
+    name: "Livermorium",
+    radioactive: true,
+    category: "Post-transition Metal"
+  },
+  Ts: {
+    name: "Tennessine",
+    radioactive: false,
+    category: "Halogen"
+  },
+  Og: {
+    name: "Oganesson",
+    radioactive: true,
+    category: "Noble Gas"
+  }
+}
+
+// Color palette
+const colors = {
+  'background': '#F2EFE9',
+  'other-non-metal': '#1AFF00',
+  'alkali-metal': '#FF0C00',
+  'alkaline-earth-metal': '#FF6700',
+  'transition-metal': '#FF54A4',
+  'metalloid': '#FFB800',
+  'post-transition-metal': '#FF762E',
+  'halogen': '#00EEFF',
+  'noble-gas': '#0E94FF',
+  'rare-earth': '#FF006A',
+  'actinoid': '#FF00C6'
+}
+
+// Viewport width
+const viewportSize = $('.full-table').width()
+
+$('#screen-width').text(viewportSize)
+
+// For every element...
+$('.element').each(function() {
+  // Append an h3 to the element with its name
+  const elementSymbol = $(this).children('.symbol').text()
+  const elementName = elementInfo[elementSymbol]['name']
+  $(this).append(`<h3 class="name">${elementName}</h3>`)
+})
+
+// Events for mobile screen sizes
+if (viewportSize <= 760) {
+  $('.element').on('click', function(){
+    // Check that the element clicked isn't already expanded
+    if ($(this).hasClass('element__expanded')) {
+      $(this).removeClass('element__expanded')
+    } else {
+      // Remove the expanded classes from whatever element has them currently (previously clicked element)
+      $('.element').removeClass('element__expanded')
+      // Add the expanded classes to the currently selected element
+      $(this).addClass('element__expanded')
+    }
+    // Show the selected element's category type in hovered mode
+    // const elementSymbol = $(this).children('.symbol').text()
+    // const elementCategory = elementInfo[elementSymbol]['category']
+    // $(this).append(`<h3 class="category">${elementCategory}</h3>`)
+  })
+}
+
+// Events for larger screen size
+if (viewportSize > 760) {
+  $('.element').removeClass('element__expanded')
+  // Helpful tip on what the corner number is for large screens
+  $('.number').attr('title', 'Atomic Number')
+
+  // Hover effects only large screens can enjoy
+  $('.element').hover(
+    function() {
+      const elementSymbol = $(this).children('.symbol').text()
+      const elementName = elementInfo[elementSymbol]['name']
+      const elementCategory = elementInfo[elementSymbol]['category']
+      const categoryColorSelector = $(this).attr('class').replace(/element /, '')
+      const groupLabelColor = colors[categoryColorSelector]
+      const isRadioactive = elementInfo[elementSymbol]['radioactive']
+        // Display hovered element's category name under the title
+      $('.group-label').text(elementCategory)
+        // Make the background of the category label box the color of the hovered category
+      $('.group-label').css('background-color', groupLabelColor)
+      //   // Add some attributes to the element to be used later
+      // $(this).attr({
+      //   "title": elementName,
+      //   "data-isRadioactive": isRadioactive
+      // })
+    },
+    // When moved from hovering this element...
+    function() {
+      // Remove the group label box and set its background color back to the page's background color
+      $('.group-label').text('')
+      $('.group-label').css('background-color', colors['background'])
+    }
+  )
+}
+
+// When an element is clicked...
+$('.element').on('click', function() {
+  const elementNumber = $(this).children('.number').text()
+  const elementName = $(this).attr("title")
+
+  // // Check to see if the clicked element is radioactive
+  if ($(this).attr("data-isRadioactive") === "true") {
+    alert(`Watch out! ${elementName} is radioactive, yo!`)
+  }
+  // Make the Periodic Videos URL
+  const videoUrl = () => {
+      let urlNumber = [...elementNumber]
+      while (urlNumber.length < 3) {
+        urlNumber.unshift('0')
+      }
+      return `http://periodicvideos.com/videos/${urlNumber.join('')}.htm`
+    }
+    // Open the video for this element in a new tab if you want to watch it
+  if (confirm(`${elementName} video?`)) {
+    window.open(videoUrl())
+  }
+})
